5667555b590e353ee13856fabeac466dd34ad6bc
Sam Reha <sreha@ucsc.edu>
2015-01-21 12:51:28 -0800

    Finalized listing form

--- models/tables.py
+++ models/tables.py
@@ -19,8 +19,8 @@ CATEGORY = ['Car', 'Bike', 'Books', 'Music', 'Outdoors', 'For the house', 'Misc.
 db.define_table('samslist',
                 Field('user_id', db.auth_user),
                 Field('name'),
-                Field('phone'),
                 Field('email'),
+                Field('phone'),
                 Field('title', unique=True),
                 Field('category'),
                 Field('price'),
@@ -34,19 +34,19 @@ db.samslist.id.readable = False
 db.samslist.user_id.default = auth.user_id
 db.samslist.user_id.writable = db.samslist.user_id.readable = False
 db.samslist.name.default = get_first_name()
-db.samslist.phone.requires = IS_MATCH('^$')
-db.samslist.phone.required = True
+db.samslist.name.writable = False
 db.samslist.email.requires = IS_EMAIL()
+db.samslist.email.default = get_email()
+db.samslist.phone.requires = IS_MATCH('^\([0-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$', error_message='Please use the format "(xxx)xxx-xxxx".')
+db.samslist.phone.required = True
 db.samslist.category.requires = IS_IN_SET(CATEGORY)
 db.samslist.category.default = 'Misc'
 db.samslist.category.required = True
-db.samslist.price.requires = IS_FLOAT_IN_RANGE(0, 100000.0, error_message='The price should be in the range 0..100000')
+db.samslist.price.requires = IS_FLOAT_IN_RANGE(0, 100000.0, error_message='The price should be in the range 0...100000')
+db.samslist.price.required = True
 db.samslist.sold_out.default = False
 db.samslist.sold_out.label = "Sold Out"
 db.samslist.date_posted.default = datetime.utcnow()
-db.samslist.name.writable = False
 db.samslist.date_posted.writable = False
 db.samslist.description.label = 'Description'
-db.samslist.price.required = True
-db.samslist.image.requires = IS_IMAGE(error_message="Oops! That's not an image file.")
-db.samslist.image.required = False
+db.samslist.image.requires = IS_EMPTY_OR(IS_IMAGE(error_message="Oops! That's not an image file."))
