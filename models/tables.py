9eb5f3e365d158571d01cbd8c3b8dee03e61ecca
Sam Reha <sreha@ucsc.edu>
2015-01-20 18:30:41 -0800

    Improved new listing form

--- models/tables.py
+++ models/tables.py
@@ -8,6 +8,12 @@ def get_first_name():
         name = auth.user.first_name
     return name
 
+def get_email():
+    email = 'None'
+    if auth.user:
+            email = auth.user.email
+    return email
+
 CATEGORY = ['Car', 'Bike', 'Books', 'Music', 'Outdoors', 'For the house', 'Misc.']
 
 db.define_table('samslist',
@@ -18,48 +24,28 @@ db.define_table('samslist',
                 Field('title', unique=True),
                 Field('category'),
                 Field('price'),
-                Field('available', 'boolean'),
+                Field('sold_out', 'boolean'),
                 Field('date_posted', 'datetime'),
                 Field('description', 'text'),
                 Field('image', 'upload'),
                 )
 
 db.samslist.id.readable = False
-db.samslist.description.label = 'Description'
-db.samslist.name.default = get_first_name()
-db.samslist.date_posted.default = datetime.utcnow()
-db.samslist.name.writable = False
-db.samslist.date_posted.writable = False
 db.samslist.user_id.default = auth.user_id
 db.samslist.user_id.writable = db.samslist.user_id.readable = False
+db.samslist.name.default = get_first_name()
+db.samslist.phone.required = True
 db.samslist.email.requires = IS_EMAIL()
 db.samslist.category.requires = IS_IN_SET(CATEGORY)
 db.samslist.category.default = 'Misc'
 db.samslist.category.required = True
+db.samslist.price.requires = IS_FLOAT_IN_RANGE(0, 100000.0, error_message='The price should be in the range 0..100000')
+db.samslist.sold_out.default = False
+db.samslist.sold_out.label = "Sold Out"
+db.samslist.date_posted.default = datetime.utcnow()
+db.samslist.name.writable = False
+db.samslist.date_posted.writable = False
+db.samslist.description.label = 'Description'
 db.samslist.price.required = True
 db.samslist.image.requires = IS_IMAGE(error_message="Oops! That's not an image file.")
-
-
-""" db.define_table('bboard',
-                Field('name'),
-                Field('user_id', db.auth_user),
-                Field('phone'),
-                Field('email'),
-                Field('category'),
-                Field('date_posted', 'datetime'),
-                Field('title'),
-                Field('bbmessage', 'text'),
-                )
-
-db.bboard.id.readable = False
-db.bboard.bbmessage.label = 'Message'
-db.bboard.name.default = get_first_name()
-db.bboard.date_posted.default = datetime.utcnow()
-db.bboard.name.writable = False
-db.bboard.date_posted.writable = False
-db.bboard.user_id.default = auth.user_id
-db.bboard.user_id.writable = db.bboard.user_id.readable = False
-db.bboard.email.requires = IS_EMAIL()
-db.bboard.category.requires = IS_IN_SET(CATEGORY)
-db.bboard.category.default = 'Misc'
-db.bboard.category.required = True """
+db.samslist.image.required = False
